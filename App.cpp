d8d5ce7d0b8953b6e435f94747990690c77a28b1
Alex Piciorus <piciorusalexandru3@gmail.com>
2022-05-31 07:56:24 +0300

    new

--- App.cpp
+++ App.cpp
@@ -15,12 +15,12 @@
 int main(int argc, char *argv[])
 {
     auto cars = std::vector<Car>{
-            Car(1,"Tesla Model S", "Tesla", "22.02.2022", 700, 250, 22, 11),
-            Car(2,"Tesla Model 3", "Tesla", "21.02.2022", 150, 250, 22, 11),
-            Car(3,"Tesla Model Y", "Tesla", "20.02.2022", 1000, 250, 22, 11),
-            Car(4,"Prius", "Toyota", "23.02.2022", 1223, 250, 22, 11),
+            Car(1,"Tesla Model S", "Tesla", "22.02.2022", 700, 250, 54, 11),
+            Car(2,"Tesla Model 3", "Tesla", "21.02.2022", 150, 250, 99, 11),
+            Car(3,"Tesla Model Y", "Tesla", "20.02.2022", 1000, 15, 44, 11),
+            Car(4,"Prius", "Toyota", "23.02.2022", 1223, 16, 12, 11),
             Car(5,"Dacia Spring", "Dacia", "24.02.2022", 1, 1, 0, 100),
-            Car(6,"E-Tron", "Audi", "25.02.2022", 150, 250, 22, 11)};
+            Car(6,"E-Tron", "Audi", "25.02.2022", 150, 18, 22, 11)};
 
     shared_ptr<CrudRepository<Car>> repo;
     repo = std::make_shared<CSVRepository>();
@@ -33,6 +33,7 @@ int main(int argc, char *argv[])
     QTableView table;
     MyModel model( controller, nullptr);
     table.resize(1210,500);
+
 
     QSortFilterProxyModel * mSortFilterModel = new QSortFilterProxyModel();
     mSortFilterModel->setDynamicSortFilter(true);
